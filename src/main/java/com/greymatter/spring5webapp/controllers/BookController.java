03bf1dd743c43d1665820c25d2185fcc794e9618
JORGE <jcgarciamed@gmail.com>
2021-04-28 16:13:05 -0500

    Adding Controllers

--- src/main/java/com/greymatter/spring5webapp/controllers/BookController.java
+++ src/main/java/com/greymatter/spring5webapp/controllers/BookController.java
@@ -1,5 +1,6 @@
 package com.greymatter.spring5webapp.controllers;
 
+import com.greymatter.spring5webapp.repositories.AuthorRepository;
 import com.greymatter.spring5webapp.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,11 +16,7 @@ public class BookController {
 
     @RequestMapping("/books")
     public String getBooks(Model model){
-        System.out.println("paso por aqui");
         model.addAttribute("books", bookRepository.findAll());
-        model.addAttribute("titulo", "Spring Framework");
-
-        System.out.println("paso por aqui 2");
         return "books/list";
     }
 
